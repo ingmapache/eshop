9583180dab7dc32c1b74899f84360671f54682da
Mapache <mapache.rgp@gmail.com>
2024-10-03 10:56:07 -0600

    ADD Routing

--- store/src/app/app.routes.ts
+++ store/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ListComponent } from './domains/Products/pages/list/list.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    }
+];
