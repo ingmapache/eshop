68988058f49332ac404cd92b8b9c8fa7664a31ab
Mapache <mapache.rgp@gmail.com>
2024-10-03 17:56:20 -0600

    ADD Class 33

--- store/src/app/domains/Products/components/product/product.component.ts
+++ store/src/app/domains/Products/components/product/product.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  @Input() img: string = '';
+  @Input() title: string = '';
+  @Input() price: number = 0;
 
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler()
+  {
+    this.addToCart.emit('Hola, huevos pto');
+  }
 }
