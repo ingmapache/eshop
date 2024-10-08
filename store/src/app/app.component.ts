2434f1601ad789707e1481e7f079835c48499151
Mapache <mapache.rgp@gmail.com>
2024-10-07 19:13:11 -0600

    ADD Class 44

--- store/src/app/app.component.ts
+++ store/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./domains/Shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: '<router-outlet />'
+  imports: [RouterOutlet, HeaderComponent],
+  template: '<div class="container mx-auto"><app-header></app-header><router-outlet /></div>'
 })
 export class AppComponent {
   title = 'store';
