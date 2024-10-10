e0281a8845430db2b4ee070b21a0f117a48b2217
Mapache <mapache.rgp@gmail.com>
2024-10-09 19:43:26 -0600

    ADD Class 54

--- store/src/app/domains/Shared/pipes/timeago.pipe.ts
+++ store/src/app/domains/Shared/pipes/timeago.pipe.ts
@@ -1,4 +1,5 @@
 import { input, Pipe, PipeTransform } from '@angular/core';
+import { formatDistance } from 'date-fns';
 
 @Pipe({
   name: 'timeAgo',
@@ -7,31 +8,11 @@ import { input, Pipe, PipeTransform } from '@angular/core';
 export class TimeAgoPipe implements PipeTransform {
 
   transform(value: string): string {
-    const currentDate = Date.now();
-    const inputDate = new Date(value).getTime();
+    
+    const dateCreated = new Date(value);
+    const actualDate = new Date();
 
-    const timeDifference = currentDate - inputDate;
-
-    const seconds = Math.floor(timeDifference / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const months = Math.floor(days / 30);
-    const years = Math.floor(days / 365);
-
-    if (years > 0) {
-      return years === 1 ? '1 year ago' : `${years} years ago`;
-    } else if (months > 0) {
-      return months === 1 ? '1 month ago' : `${months} months ago`;
-    } else if (days > 0) {
-      return days === 1 ? '1 day ago' : `${days} days ago`;
-    } else if (hours > 0) {
-      return hours === 1 ? '1 hour ago' : `${hours} hours ago`;
-    } else if (minutes > 0) {
-      return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`;
-    } else {
-      return seconds === 1 ? '1 second ago' : `${seconds} seconds ago`;
-    }
+    return 'Added ' + formatDistance(actualDate, dateCreated) + ' ago.';
   }
 
 }
