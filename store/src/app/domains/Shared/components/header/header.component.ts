0b622f04fdadc142f14861b859466cce3aafcf03
Mapache <mapache.rgp@gmail.com>
2024-10-10 12:39:01 -0600

    ADD Class 58

--- store/src/app/domains/Shared/components/header/header.component.ts
+++ store/src/app/domains/Shared/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkWithHref],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
