08bea57736b053b452f17ce3673871b9e690629d
Mapache <mapache.rgp@gmail.com>
2024-10-10 13:19:02 -0600

    ADD Class 59

--- store/src/app/app.routes.ts
+++ store/src/app/app.routes.ts
@@ -3,14 +3,21 @@ import { Routes } from '@angular/router';
 import { ListComponent } from '@products/pages/list/list.component';
 import { AboutComponent } from './domains/Info/pages/about/about.component';
 import { NotFoundComponent } from './domains/Info/pages/not-found/not-found.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 export const routes: Routes = [
     {
+        path:'',
+        component: LayoutComponent,
+        children: [
+            {
         path: '',
         component: ListComponent
     },
     {
         path: 'about',
         component: AboutComponent
+    }
+        ]
     },
     {
         path: '**',
