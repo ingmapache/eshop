3ce39ef1ff53d9db301c27a57fb3ec3d33369aaf
Mapache <mapache.rgp@gmail.com>
2024-10-11 15:13:16 -0600

    ADD Clas 69 Preload

--- store/src/app/app.config.ts
+++ store/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: 
   [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withPreloading(PreloadAllModules)),
     provideHttpClient()
   ]
 };
